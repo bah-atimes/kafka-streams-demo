0fce7c462d2ed7590ca529d6baad3f3de70cb5b0
atimes <582309@bah.com>
2020-08-19 10:56:53 -0400

    Workitem 776134 Added the mother of all unit tests. . .

--- src/test/java/gov/va/vba/kafka/streams/demo/AppTest.java
+++ src/test/java/gov/va/vba/kafka/streams/demo/AppTest.java
@@ -1,20 +1,26 @@
 package gov.va.vba.kafka.streams.demo;
 
-import static org.junit.Assert.assertTrue;
-
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+    @InjectMocks
+    App app;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testTheApp() {
+        String arr[] = new String[]{"something"};
+        app.main(arr);
     }
 }
