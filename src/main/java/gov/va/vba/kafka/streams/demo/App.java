3b95fc1a0897fd7f3d37c1703e59d9113b09e3f2
atimes <582309@bah.com>
2020-08-19 10:28:01 -0400

    Workitem 776134 initial commit

---
+++ src/main/java/gov/va/vba/kafka/streams/demo/App.java
@@ -0,0 +1,45 @@
+package gov.va.vba.kafka.streams.demo;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+
+import java.util.*;
+
+public class App
+{
+    private List<String> inputTopic = Arrays.asList("sync-claim-topic", "sync-person-topic");
+    private String bootstrapServers = "broker:9092";
+
+    public static void main( String[] args )
+    {
+        new App();
+    }
+
+    public App() {
+        Properties streamsConfiguration = new Properties();
+
+        //ensure the stream always start at the beginning
+        streamsConfiguration.put(StreamsConfig.APPLICATION_ID_CONFIG, UUID.randomUUID().toString());
+        streamsConfiguration.put(ConsumerConfig.GROUP_ID_CONFIG, UUID.randomUUID().toString());
+        streamsConfiguration.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        streamsConfiguration.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+
+        streamsConfiguration.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        streamsConfiguration.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG,Serdes.String().getClass().getName());
+
+        StreamsBuilder builder = new StreamsBuilder();
+        KStream<String, String> lines = builder.stream(inputTopic);
+
+        lines.foreach((key, value) -> System.out.println("Value:" + value));
+
+        KafkaStreams streams = new KafkaStreams(builder.build(), new StreamsConfig(streamsConfiguration));
+        streams.start();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
+    }
+}
